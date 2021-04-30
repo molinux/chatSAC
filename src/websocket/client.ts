0c8addb1826f70d5c6f9f5ddd83115f6052ed437
Molinux <cybercrowbr@gmail.com>
2021-04-29 21:31:19 -0300

    Building admin chat interface, but not finished yet.

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -49,9 +49,10 @@ io.on('connect', (socket) => {
     await messagesService.create({
       text,
       user_id
-    })
+    });
 
+    const allMessages = await messagesService.listByUser(user_id);
 
-    // Salvar a conexão com o socket_id, user_id, 
-  })
+    socket.emit("client_list_all_messages", allMessages);
+  });
 });
